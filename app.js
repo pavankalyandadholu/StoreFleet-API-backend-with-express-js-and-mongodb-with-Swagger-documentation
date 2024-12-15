553c6ea6cd87fd7fd0779ae27783878e6488b4d8
Officerdhruva <dpavankalyan8887@gmail.com>
2024-12-15 22:41:34 +0530

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cors from 'cors'
 import productRoutes from "./src/product/routes/product.routes.js";
 import {
   errorHandlerMiddleware,
@@ -15,6 +16,7 @@ import { setupSwagger } from "./swagger.js";
 dotenv.config({ path: configPath });
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 setupSwagger(app);
